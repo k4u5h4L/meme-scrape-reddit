0cdb870ae0efd678bba8531fe434826a96a2962f
Kaushal <kaushal.v.bhat@gmail.com>
2020-07-05 18:37:19 +0530

    add scraper

---
+++ index.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const mainUrl = `https://reddit.com/r/dankmemes`;
+axios
+    .get(mainUrl)
+    .then((response) => {
+        dealWithData(response.data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+const dealWithData = (html) => {
+    const $ = cheerio.load(html);
+    const urlMeme = $("._2_tDEnGMLxpM6uOa2kaDB3.ImageBox-image.media-element._1XWObl-3b9tPy64oaG6fax");
+    const indexValue = randNo(10);
+    console.log(`Source is:\n${urlMeme[indexValue].attribs.src}`);
+};
+const randNo = (limit) => {
+    const thatNo = Math.floor(Math.random() * limit);
+    return thatNo;
+};
